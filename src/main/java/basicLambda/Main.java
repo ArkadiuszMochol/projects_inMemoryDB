c036435a7d5530b5f67e72d7980fd5c2e0a8a409
Arkadiusz Mochol <mochol_arkadiusz@o2.pl>
2020-05-05 22:36:30 +0200

    Created getAllProjectOrderByBudgetDesc in ProjectController class

--- src/main/java/basicLambda/Main.java
+++ src/main/java/basicLambda/Main.java
@@ -1,10 +1,15 @@
 package basicLambda;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ProjectController pc = new ProjectController();
 
-        pc.getAllProjects();
-        pc.getProjectAcronimAndBudget();
+        //pc.getAllProjects();
+       // pc.getProjectNameAndBudget();
+        pc.getAllProjectOrderByBudgetDesc().forEach(System.out::println);
     }
 }
--- src/main/java/basicLambda/ProjectController.java
+++ src/main/java/basicLambda/ProjectController.java
@@ -2,23 +2,32 @@ package basicLambda;
 
 import java.sql.SQLOutput;
 import java.text.NumberFormat;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ProjectController {
-//1. Metoda wypisująca wszystkie projekty
-public void getAllProjects(){
-    InMemoryDB.projects.stream()
-            .forEach(project -> System.out.println(project));
-}
-//2. Metoda zwracajaca Nazwę oraz budżet projektu 
-public void getProjectAcronimAndBudget(){
-    Locale locale = new Locale("pl","PL");
-    NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
-    InMemoryDB.projects.stream()
-            .forEach(project -> System.out.printf("Nazwa: %-30s || Budget: %20s \n",
-                    project.getName(), currencyFormatter.format(project.getBudget())));
-}
+    //1. Metoda wypisująca wszystkie projekty
+    public void getAllProjects() {
+        InMemoryDB.projects.stream()
+                .forEach(project -> System.out.println(project));
+    }
 
+    //2. Metoda zwracajaca Nazwę oraz budżet projektu
+    public void getProjectNameAndBudget() {
+        Locale locale = new Locale("pl", "PL");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        InMemoryDB.projects.stream()
+                .forEach(project -> System.out.printf("Nazwa: %-30s || Budget: %20s \n",
+                        project.getName(), currencyFormatter.format(project.getBudget())));
+    }
+//3. Metoda zwracająca listę projektów posortowanych po budżecie max->min
+    public List<Project> getAllProjectOrderByBudgetDesc(){
+        return InMemoryDB.projects.stream()
+                .sorted(Comparator.comparingDouble(Project::getBudget).reversed())
+                .collect(Collectors.toList());
+    }
 
 
 }
