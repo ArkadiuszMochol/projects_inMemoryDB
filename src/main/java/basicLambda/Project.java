7542fa50a33bbb57bbf51ea514b90d4a2326b7d0
Arkadiusz Mochol <mochol_arkadiusz@o2.pl>
2020-05-05 21:22:07 +0200

    Define object Project in the class Project

--- src/main/java/basicLambda/Project.java
+++ src/main/java/basicLambda/Project.java
@@ -1,4 +1,21 @@
 package basicLambda;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+
 public class Project {
+    private String name;
+    private double budget;
+    private int noEmployees;
+    private double percentOfFounds;
+    private List<Category> categories;
+
+    public double getProjectFouns(){
+        return (budget*percentOfFounds)/100;
+    }
+
 }
